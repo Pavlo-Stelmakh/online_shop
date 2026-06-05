424d2511c265055f5398645033cc2676e6e36df8
Pavel Stelmakh <pavelstelmah780@gmail.com>
2026-06-05 14:34:09 +0300

    Use DATABASE_URL for Alembic migrations

--- alembic/env.py
+++ alembic/env.py
@@ -1,13 +1,23 @@
+import os
 from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
-
 from alembic import context
+from dotenv import load_dotenv
 
-# this is the Alembic Config object, which provides
-# access to the values within the .ini file in use.
+from database import Base
+from models import Category, Product, Customer, Order, OrderItem, User
+
+
 config = context.config
+
+load_dotenv()
+
+database_url = os.getenv("DATABASE_URL")
+
+if database_url:
+    config.set_main_option("sqlalchemy.url", database_url)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
